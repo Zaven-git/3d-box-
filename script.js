bbbc899ae0678dc8caf5924fa45755e1e01151f9
zaven-git <khzaven@gmail.com>
2019-09-18 14:25:00 +0400

    3d box with html css and javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+
+    let rotate = document.querySelectorAll('.rotate');
+    let rotateSides = ['X', 'Y', 'Z'];
+    let container = document.querySelector('.container');
+
+    rotate.forEach(function(item, i){
+        item.addEventListener('click', ()=>{
+            container.style.animation = `rotate${rotateSides[i]} 10s infinite linear`;
+        });
+    });
+
+});
